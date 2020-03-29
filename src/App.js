ad0ad229b2e093de2c877d62ab27027dafb71bf2
Syd Hashemi <hhashemigh@gmail.com>
2020-03-29 00:31:28 -0700

    update menu-items to navigate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.compenent.jsx'
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+)
 
 function App() {
   return (
-    <div> <HomePage/> </div>
+    <div>
+    <switch>
+    
+    <Route exact path='/' component= {HomePage} />
+    <Route path='/shop/hats' component= {HatsPage} />
+    </switch>
+    </div>
   );
 }
 
