6afa4bbaf1443e85e09b35a3789290a035bf6108
Syd Hashemi <hhashemigh@gmail.com>
2020-03-29 18:48:22 -0700

    adding header component and links

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.compenent.jsx'
 import ShopPage from './pages/shop/shop.components.jsx'
-
+import Header from './components/header/header.component.jsx'
 
 function App() {
   return (
     <div>
+    <Header/>
     <switch>
     
-    <Route exact path='/' component= {HomePage} />
-    <Route path='/shop' component= {ShopPage} />
+      <Route exact path='/' component= {HomePage} />
+      <Route path='/shop' component= {ShopPage} />
     </switch>
     </div>
   );
